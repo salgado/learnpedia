6a68252e5887d9659690baa0ea418a08d20cbe0a
Lucas_Sorrentino <lksorrentino@gmail.com>
2018-03-01 11:01:32 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learnpedia::Application.config.secret_token = 'd9c76abf0fb70e4bb8695eaa83292f2cba99ccadde4fcadb4faa3307174a206bee20911dc4a86a1f443533b317a320fe7d9d169939ca41219b916a7e89c2b73f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Learnpedia::Application.config.session_store :cookie_store, key: '_learnpedia_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Learnpedia::Application.config.session_store :active_record_store
